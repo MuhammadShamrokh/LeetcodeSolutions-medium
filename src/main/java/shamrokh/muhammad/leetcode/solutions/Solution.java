cc52edc2302b40ce29cee2c388c284a95723be13
MuhammadShamrokh <Muhammad.comp1996@gmail.com>
2024-10-03 18:54:21 +0300

    15. 3Sum

--- src/main/java/shamrokh/muhammad/leetcode/solutions/Solution.java
+++ src/main/java/shamrokh/muhammad/leetcode/solutions/Solution.java
@@ -1,64 +1,50 @@
 package shamrokh.muhammad.leetcode.solutions;
+import java.util.*;
 
-class Solution {
-    public String intToRoman(int num) {
-        StringBuilder sb = new StringBuilder();
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
 
-        while(num !=0){
-            if(num >= 1000){
-                sb.append("M");
-                num-=1000;
-            }
-            else if(num >= 900){
-                sb.append("CM");
-                num -= 900;
-            }
-            else if(num >= 500){
-                sb.append("D");
-                num -= 500;
-            }
-            else if(num >= 400){
-                sb.append("CD");
-                num-=400;
-            }
-            else if(num >= 100){
-                sb.append("C");
-                num-=100;
-            }
-            else if(num >= 90){
-                sb.append("XC");
-                num -= 90;
-            }
-            else if(num >= 50){
-                sb.append("L");
-                num-=50;
-            }
-            else if(num>=40){
-                sb.append("XL");
-                num-=40;
-            }
-            else if(num>=10){
-                sb.append("X");
-                num-=10;
-            }
-            else if(num==9){
-                sb.append("IX");
-                num-=9;
-            }
-            else if(num>=5){
-                sb.append("V");
-                num-=5;
-            }
-            else if(num==4){
-                sb.append("IV");
-                num-=4;
-            }
-            else if(num>=1){
-                sb.append("I");
-                num -=1;
-            }
+        // Step 1: Sort the array
+        Arrays.sort(nums);
+
+        // Step 2: Iterate through the array
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for the current element
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            // Step 3: Use two-pointer technique for finding the other two elements
+            twoSum(nums, i, result);
         }
 
-        return sb.toString();
+
+        return result;
+    }
+
+    private void twoSum(int[] nums, int i, List<List<Integer>> result){
+        int left = i + 1;
+        int right = nums.length - 1;
+        int target = -nums[i];
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+
+            if (sum == target) {
+                result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                // Skip duplicates for the second and third elements
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+
+                // Move both pointers
+                left++;
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
     }
 }
--- src/test/java/shamrokh/muhammad/leetcode/solutions/SolutionTest.java
+++ src/test/java/shamrokh/muhammad/leetcode/solutions/SolutionTest.java
@@ -19,21 +19,18 @@ class SolutionTest {
     @Order(1)
     @DisplayName("Test one")
     public void testOne(){
-        assertEquals("MMMDCCXLIX", solution.intToRoman(3749));
     }
 
     @Test
     @Order(2)
     @DisplayName("Test Two")
     public void testTwo(){
-        assertEquals("LVIII", solution.intToRoman(58));
     }
 
     @Test
     @Order(3)
     @DisplayName("Test Three")
     public void testThree(){
-        assertEquals("MCMXCIV", solution.intToRoman(1994));
     }
 
     @Test
